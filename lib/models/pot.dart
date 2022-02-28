df3156ebe5edcde5476fdf24ebf91bb29b221c46
Фаиз <galimow63@gmail.com>
2022-02-28 22:33:31 +0400

    adding pot model, managing folders

---
+++ lib/models/pot.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/foundation.dart';
+
+class Pot {
+  String name;
+  double percent;
+  double amount;
+
+  Pot({
+    @required this.name,
+    @required this.percent,
+    @required this.amount,
+  });
+}
---
+++ lib/pages/pot_detail_page.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class PotDetailPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
