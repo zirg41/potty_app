179654584810bfff2c588f7e970243feb6078330
Фаиз <galimow63@gmail.com>
2022-02-28 22:20:58 +0400

    empty project

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,6 @@ void main() => runApp(const MyApp());
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -13,49 +12,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primaryColor: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Potty App'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  TextEditingController incomeField = TextEditingController();
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Potty"),
-        backgroundColor: Theme.of(context).primaryColor,
-      ),
-      body: Column(
-        children: [
-          Container(
-            padding: const EdgeInsets.all(8),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                const Text(
-                  "Введите доход, руб",
-                  style: TextStyle(fontSize: 18),
-                ),
-                TextField(
-                  controller: incomeField,
-                ),
-              ],
-            ),
-          )
-        ],
-      ),
+      home: Container(),
     );
   }
 }
