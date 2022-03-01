b0a7cd85b6918a78892bb3de9afb82285c501478
Фаиз <galimow63@gmail.com>
2022-03-01 23:28:09 +0400

    PotItem managed

--- lib/widgets/pot_item.dart
+++ lib/widgets/pot_item.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:potty_app/models/pot.dart';
+import 'package:flutter/services.dart';
+
+import '/models/pot.dart';
 
 class PotItem extends StatelessWidget {
   final Pot pot;
@@ -7,8 +9,78 @@ class PotItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    const itemsPadding = EdgeInsets.all(10);
+    final ctxTheme = Theme.of(context);
+    const snackBar = SnackBar(
+      content: Text('Скопировано!'),
+      duration: Duration(seconds: 1),
+    );
+
     return Card(
-      child: Text(pot.name),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Row(
+            children: [
+              GestureDetector(
+                onTap: () {},
+                child: Container(
+                  // ПРОЦЕНТЫ
+                  margin: EdgeInsets.all(8),
+                  decoration: BoxDecoration(
+                      color: (pot.name != "Не распределено") &&
+                              (pot.name != "Перераспределение")
+                          ? ctxTheme.primaryColor
+                          : pot.name == "Перераспределение"
+                              ? ctxTheme.errorColor
+                              : ctxTheme.focusColor,
+                      //border: Border.all(width: 8),
+                      borderRadius: BorderRadius.circular(10)),
+                  padding: itemsPadding,
+                  child: Text(
+                    "${pot.percent.toStringAsFixed(0)} %",
+                    style:
+                        const TextStyle(fontSize: 18, color: Color(0xFFf4f1de)),
+                  ),
+                ),
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  GestureDetector(
+                    onTap: () {
+                      Clipboard.setData(
+                          ClipboardData(text: pot.amount.toString()));
+                      ScaffoldMessenger.of(context).showSnackBar(snackBar);
+                    },
+                    child: Container(
+                      // СУММА
+                      margin: EdgeInsets.all(5),
+                      //padding: itemsPadding,
+                      child: Text(
+                        pot.amount != null ? pot.amount.toString() : "-",
+                        style: const TextStyle(fontSize: 18),
+                      ),
+                    ),
+                  ),
+                  Container(
+                    // НАИМЕНОВАНИЕ
+                    //padding: itemsPadding,
+                    margin: const EdgeInsets.only(
+                        left: 5, right: 5, top: 0, bottom: 5),
+                    child: FittedBox(
+                      child: Text(
+                        pot.name,
+                        style: const TextStyle(fontSize: 16),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
