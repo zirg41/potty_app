cc5f7f03a061bc062a51e0e711c1c33bc552947c
Фаиз <galimow63@gmail.com>
2022-03-09 23:41:03 +0400

    isAmountFixed added

--- lib/models/pot.dart
+++ lib/models/pot.dart
@@ -1,15 +1,17 @@
 import 'package:flutter/foundation.dart';
 
 class Pot {
-  String id;
+  final String id;
   String name;
   double percent;
   double amount;
+  bool isAmountFixed;
 
   Pot({
     @required this.id,
     @required this.name,
     this.percent,
     this.amount,
+    this.isAmountFixed = false,
   });
 }
