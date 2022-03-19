148448ee6260ea0a36fe7edefdbb9e81b4bcd7b0
Фаиз <galimow63@gmail.com>
2022-03-19 14:58:59 +0400

    sqlite stuff

--- lib/helpers/db_helper.dart
+++ lib/helpers/db_helper.dart
@@ -1,1 +1,30 @@
-class DBHelper {}
+import 'package:sqflite/sqflite.dart' as sql;
+import 'package:path/path.dart' as path;
+
+class DBHelper {
+  static Future<sql.Database> potSetDatabase() async {
+    final dbPath = await sql.getDatabasesPath();
+    return sql.openDatabase(
+      path.join(dbPath, 'pot_sets.db'),
+      version: 1,
+      onCreate: (db, version) {
+        return db.execute(
+          "CREATE TABLE user_pot_sets(id TEXT PRIMARY KEY, title TEXT, income TEXT)",
+        );
+      },
+    );
+  }
+
+  static Future<sql.Database> potsDatabase() async {
+    final dbPath = await sql.getDatabasesPath();
+    return sql.openDatabase(
+      path.join(dbPath, 'pots.db'),
+      version: 1,
+      onCreate: (db, version) {
+        return db.execute(
+          "CREATE TABLE user_pots(id TEXT PRIMARY KEY, pot_set_id TEXT, name TEXT, )",
+        );
+      },
+    );
+  }
+}
