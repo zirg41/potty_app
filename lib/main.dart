6b38a6cf5fd0e9bc36b2c65570d8dd080ea058e4
Фаиз <galimow63@gmail.com>
2022-03-19 15:23:01 +0400

    setuping hive initialization

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:potty_app/providers/pot_set.dart';
 import 'package:provider/provider.dart';
+import 'package:hive/hive.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
 import 'config/routes/routes.dart';
 import 'config/themes/light_theme.dart';
 
 import 'pages/pots_collection_page.dart';
-import 'pages/pot_set_page.dart';
 
 import 'providers/pots.dart';
 
-void main() => runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Hive.initFlutter();
+
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
   const MyApp();
