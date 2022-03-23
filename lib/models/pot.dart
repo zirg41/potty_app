db2984774c6a51722cc583308858f600eae68bc9
Фаиз <galimow63@gmail.com>
2022-03-23 22:09:52 +0400

    adding Hive annotations to Pot model

--- lib/models/pot.dart
+++ lib/models/pot.dart
@@ -1,10 +1,22 @@
 import 'package:flutter/foundation.dart';
+import 'package:hive/hive.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
-class Pot {
+@HiveType(typeId: 0)
+class Pot extends HiveObject {
+  @HiveField(0)
   final String id;
+
+  @HiveField(1)
   String name;
+
+  @HiveField(2)
   double percent;
+
+  @HiveField(3)
   double amount;
+
+  @HiveField(4)
   bool isAmountFixed;
 
   Pot({
