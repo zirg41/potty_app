0db9d2dc3a8b8dfd838c90668d922a9fb4694ce8
Фаиз <galimow63@gmail.com>
2022-03-23 22:09:52 +0400

    adding button to exit from test mode

--- lib/pages/test_hive_page.dart
+++ lib/pages/test_hive_page.dart
@@ -1,16 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:potty_app/pages/pots_collection_page.dart';
 
 class TestHivePage extends StatelessWidget {
-  const TestHivePage();
-
-  void doSomething() {
-    // print("test");
+  TestHivePage() {
+    Hive.initFlutter();
   }
+
+  void doSomething() {}
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("Test Hive Page")),
+      appBar: AppBar(
+        title: const Text("Test Hive Page"),
+        actions: [
+          IconButton(
+            onPressed: () => Navigator.of(context)
+                .pushReplacementNamed(PotsCollectionPage.routeName),
+            icon: const Icon(Icons.exit_to_app_outlined),
+          )
+        ],
+      ),
       body: Container(),
       floatingActionButton: FloatingActionButton(
         child: const Icon(Icons.add),
