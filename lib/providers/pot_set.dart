a6efefd3760ca37a5c5007c848c446b5fe27366f
Фаиз <galimow63@gmail.com>
2022-03-23 22:09:52 +0400

    adding Hive annotations to PotSet model

--- lib/providers/pot_set.dart
+++ lib/providers/pot_set.dart
@@ -1,12 +1,28 @@
 import 'package:flutter/cupertino.dart';
+import 'package:hive/hive.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 import 'package:potty_app/models/pot.dart';
 
-class PotSet with ChangeNotifier {
+part 'pot_set.g.dart';
+
+@HiveType(typeId: 1)
+class PotSet extends HiveObject with ChangeNotifier {
+  @HiveField(0)
   final String id;
+
+  @HiveField(1)
   double income;
+
+  @HiveField(2)
   String name;
+
+  @HiveField(3)
   List<Pot> pots;
+
+  @HiveField(4)
   double unallocatedAmount = 0;
+
+  @HiveField(5)
   double unallocatedPercent = 0;
 
   PotSet({
